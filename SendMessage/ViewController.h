0e5fdae385af9491c82e2bb9d040bc1b851f564e
Anuj Katiyal <anujk3@gmail.com>
2014-09-25 16:40:07 +0530

    Initial Commit

---
+++ SendMessage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SendMessage
+//
+//  Created by Anuj Katiyal on 25/09/14.
+//  Copyright (c) 2014 Katiyals. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SendMessage/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SendMessage
+//
+//  Created by Anuj Katiyal on 25/09/14.
+//  Copyright (c) 2014 Katiyals. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
